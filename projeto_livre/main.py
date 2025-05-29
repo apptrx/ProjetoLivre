2b0aa3cd3f805ff19b7102b73f5897d319c1b06b
apptrx <matheusscamargo13@gmail.com>
2025-05-29 08:45:26 -0300

    Update main.py

--- projeto_livre/main.py
+++ projeto_livre/main.py
@@ -1,23 +1,41 @@
-from package.usuario import usuario
+from package.usuario import Usuario
 from dados import biblioteca
-from package.utilizar import carregar_dados 
+from package.utilizar import carregar_dados, salvar_dados 
+from package.funcionario import Funcionario
 
 dados = carregar_dados('dados/biblioteca.json')
+# Carrega dados do JSON
+
 usuarios = [usuario.trans_objeto(d) for d in dados['usuarios']]
+# Converte dicionário em objeto 
 
 print("Bem-vindo ao sistema de gerenciamento de usuários!")
-escolha = input("Por favor, fará o login ou registro? ")
+escolha = input("Por favor, digite 'login' ou 'registro: ")
 if escolha.lower() == 'registro':
     nome_input = input("Digite seu nome de usuário: ")
     senha_input = input("Digite sua senha: ")
-    novo_usuario = usuario(nome_input, senha_input)
+    novo_usuario = Usuario(nome_input, senha_input)
     usuarios.append(novo_usuario)
     dados['usuarios'].append(novo_usuario.volta_objeto())
+    salvar_dados('dados/biblioteca.json', dados)
+    print("Usuário criado com sucesso!")
 elif escolha.lower() == 'login':
     nome_input = input("Usuário: ")
     senha_input = input("Senha: ")
-    usuario = usuario(nome_input, senha_input)
-    if usuario.autenticar():
+    usuario = Usuario(nome_input, senha_input)
+    
+    if usuario.autenticar(usuarios):
         print("Login realizado com sucesso!")
+        if usuario.tipo == "funcionario":
+            print("Bem-vindo, funcionário!")
+            funcionario = Funcionario(nome_input, senha_input)
+            titulo = input("Digite o título do livro: ")
+            autor = input("Digite o autor do livro: ")
+            ano = input("Digite o ano de publicação: ")
+            funcionario.cadastrar_livro(titulo, autor, ano)
+        else:
+            print("Bem-vindo, usuário comum!")
     else:
         print("Falha no login. Verifique seu nome de usuário e senha.")
+else:
+    print("Opção inválida. Por favor, escolha 'login' ou 'registro'.")
