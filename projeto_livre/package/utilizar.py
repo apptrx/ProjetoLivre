aaad3f35e697a3bd6ca0039e9cd68f4917e9d4b8
apptrx <matheusscamargo13@gmail.com>
2025-05-30 20:43:15 -0300

    tentando recuperar o sinal

--- projeto_livre/package/utilizar.py
+++ projeto_livre/package/utilizar.py
@@ -3,19 +3,17 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Funções para carregar e salvar dados em JSON
-
 def carregar_dados(caminho_relativo):
     caminho = os.path.join(BASE_DIR, caminho_relativo)
     if not os.path.exists(caminho):
-        with open(caminho, 'w') as f:
-            json.dump({"usuarios": [], "livros": []}, f, indent=4)
-    with open(caminho, 'r') as f:
+        with open(caminho, 'w', encoding='utf-8') as f:
+            json.dump({"usuarios": [], "livros": []}, f, indent=4, ensure_ascii=False)
+    with open(caminho, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 def salvar_dados(caminho_relativo, dados):
     caminho = os.path.join(BASE_DIR, caminho_relativo)
-    with open(caminho, 'w') as f:
+    with open(caminho, 'w', encoding='utf-8') as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
 
     
